2c87697ebf2ae68400a24d48aec75fe8c8014646
Modester-mwa <70933725+Modester-mw@users.noreply.github.com>
2021-01-15 09:16:00 +0200

    Kemoda

---
+++ MyFirstClass.java
@@ -0,0 +1,31 @@
+package day1.Examples;
+
+
+/*
+ * This will be my first java class
+ * and first java program
+ */
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Modester's first java program");
+		
+		// TODO Auto-generated method stub
+		
+/*
+ * This is question two
+ * calculating Parameter and surface
+ * area
+ */
+		int width = 24;	
+		int length = 30;
+		int result = 2 * (width + length);
+		System.out.println(result);
+		
+		
+		
+
+	}
+
+}
